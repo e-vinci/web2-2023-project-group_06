22e94a926411e0b8a993fa27480982327622e6bc
guillaume-kennes-vinci <114008498+guillaume-kennes-vinci@users.noreply.github.com>
2023-11-07 08:49:22 +0100

    add homepage stuff

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,24 @@
 const HomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+  
+  const mainfiller = `
+  <header class="text-center"><div id="navbarWrapper"></div>
+        
+        <h1>Welcome to Boonder</h1>
+      </header>
+
+      <div class="text-center">
+        The place to meet new books <br>
+        Like  Swipe Match <br>
+        <button type="button" class="btn btn-primary">Login</button> <br>
+        New here ? <button type="button" class="btn btn-primary">Sign in</button>
+      </div>
+  `;
+
+  main.innerHTML = mainfiller;
+
+
 };
+
 
 export default HomePage;
---
+++ frontend/src/Components/Pages/LogInPage.js
@@ -0,0 +1,18 @@
+const LogInPage = () => {
+    const main = document.querySelector('main');
+    const mainfiller = `
+    <header class="text-center"><div id="navbarWrapper"></div>
+            
+            <h1>Welcome to Boonder</h1>
+        </header>
+    
+        <div class="text-center">
+            
+        </div>
+    `;
+    
+    main.innerHTML = mainfiller;
+};
+    
+export default LogInPage;
+  
