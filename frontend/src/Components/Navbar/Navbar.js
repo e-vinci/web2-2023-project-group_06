7603123ee95e26bcce70c56f6f371ed7bac75e75
Kornel25 <kornel.ambrozewski@student.vinci.be>
2023-11-07 09:19:11 +0100

    Supression du bouton Home et remplacement par le le Boonder qui nous redirige vers la HomePage

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -13,7 +13,8 @@ const Navbar = () => {
   const navbar = `
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Boonder</a>
+        <a class="navbar-brand" href="#" data-uri="/">Boonder</a>
+
           <button
             class="navbar-toggler"
             type="button"
@@ -27,9 +28,6 @@ const Navbar = () => {
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
-              </li>
               <li class="nav-item">
                 <a class="nav-link" href="#" data-uri="/game">Game</a>
               </li>
