53f2e2a32feae68cc61b2fa801e69ba5e35b738a
Kornel25 <kornel.ambrozewski@student.vinci.be>
2023-11-07 10:03:46 +0100

    Ajustement des boutons pour qu'ils soient pas cliquable tout le long de la ligne

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -2,27 +2,24 @@ const HomePage = () => {
   const main = document.querySelector('main');
   
   const mainfiller = `
-  <header class="text-center"><div id="navbarWrapper"></div>
-        
-        <h1>Welcome to Boonder</h1>
-      </header>
+  <div class="text-center">
+  The place to meet new books <br>
+  Like Swipe Match <br>
+  <button type="button" class="btn btn-primary">
+    <a class="nav-link" href="/login">Login</a>
+  </button>
+  <br>
+  <button type="button" class="btn btn-primary">
+    <a class="nav-link" href="/signin">Sign in</a>
+  </button>
+</div>
+<footer>
+  <button type="button" class="btn btn-primary">
+    <a class="nav-link" href="/about">About Us</a>
+  </button>
+  <br>
+</footer>
 
-      <div class="text-center">
-        The place to meet new books <br>
-        Like  Swipe Match <br>
-
-        <a class="nav-link" href="/login">
-        <button type="button" class="btn btn-primary">Login</button> <br>
-        </a>
-        <a class="nav-link" href="/signin">
-        New here ? <button type="button" class="btn btn-primary">Sign in</button>
-      </a>
-        </div>
-        <footer>
-        <a class="nav-link" href="/about">
-        <button type="button" class="btn btn-primary">About Us</button> <br>
-        </a>
-        </footer>
   `;
 
   main.innerHTML = mainfiller;
