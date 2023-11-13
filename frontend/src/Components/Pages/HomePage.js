6c70fada46e0e8de90f415299f649bfe858c05f6
guillaume-kennes-vinci <114008498+guillaume-kennes-vinci@users.noreply.github.com>
2023-11-13 14:34:37 +0100

    add profil page

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -2,24 +2,30 @@ const HomePage = () => {
   const main = document.querySelector('main');
   
   const mainfiller = `
-  <div class="text-center">
-  The place to meet new books <br>
-  Like Swipe Match <br>
-  <button type="button" class="btn btn-primary">
-    <a class="nav-link" href="/login">Login</a>
-  </button>
-  <br>
-  <button type="button" class="btn btn-primary">
-    <a class="nav-link" href="/signin">Sign in</a>
-  </button>
-</div>
-<footer>
-  <button type="button" class="btn btn-primary">
-    <a class="nav-link" href="/about">About Us</a>
-  </button>
-  <br>
-</footer>
+  <header class="text-center"><div id="navbarWrapper"></div>
+    <h1>Welcome to Boonder</h1>
+  </header>
 
+      <div class="text-center">
+        The place to meet new books <br>
+        Like  Swipe Match <br>
+        
+        <button type="button" class="btn btn-primary">
+          <a class="nav-link" href="/login">Login</a>
+        </button> <br>
+        
+        New here ? 
+        <button type="button" class="btn btn-primary">
+          <a class="nav-link" href="/signin">Sign in</a>
+        </button>
+        
+        <footer>
+          <button type="button" class="btn btn-primary">
+            <a class="nav-link" href="/about">About Us</a>
+          </button>
+          <br>
+        </footer>
+      </div>
   `;
 
   main.innerHTML = mainfiller;
