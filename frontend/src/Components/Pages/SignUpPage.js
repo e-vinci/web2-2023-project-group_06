62b42e8b5efe62ebbb83489383671c64b7c5efc1
Kornel25 <kornel.ambrozewski@student.vinci.be>
2023-12-09 17:19:02 +0100

    Gestion des erreurs du mot de passe ( mini 8 char, 1 majuscule, 1 chiffre)

--- frontend/src/Components/Pages/SignUpPage.js
+++ frontend/src/Components/Pages/SignUpPage.js
@@ -5,27 +5,40 @@ const SignUpPage = () => {
       <div id="navbarWrapper"></div>    
       <h1>Welcome to Boonder</h1>
     </header>
-    <div class="container login-container">
-      <form id="signup-form">
-        <div class="col-lg-4 mx-auto">
-          <div class="form-group">
-            <input type="text" class="form-control mb-2" id="email" placeholder="Email address">
-          </div>
+    <div class="container mt-5">
+      <div class="row justify-content-md-center">
+        <div class="col-md-6">
+          <form id="signup-form" class="needs-validation" novalidate>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="email" placeholder="Email address" required>
+              <div class="invalid-feedback">
+                Please provide a valid email.
+              </div>
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password" placeholder="Password" minlength="8" pattern="^(?=.*[A-Z])(?=.*[0-9]).*$" required>
+              <div class="invalid-feedback">
+                Votre mot de passe n'est pas conforme avec les conditions !
+              </div>
+              <div class="form-text">
+                Votre mot de passe doit avoir au moins 8 caractères dont 1 majuscule et 1 caractère alphanumérique
+              </div>
+            </div>
+            <div class="mb-3">
+              <label for="confirmPassword" class="form-label">Password Confirmation</label>
+              <input type="password" class="form-control" id="confirmPassword" placeholder="Password Confirmation" required>
+              <div class="invalid-feedback">
+                Passwords do not match.
+              </div>
+            </div>
+            <div class="text-center">
+              <button type="submit" class="btn btn-primary btn-block myButton">Sign Up</button>
+            </div>
+          </form>
         </div>
-        <div class="col-lg-4 mx-auto">
-          <div class="form-group">
-            <input type="password" class="form-control mb-2" id="password" placeholder="Password">
-          </div>
-        </div>
-        <div class="col-lg-4 mx-auto">    
-          <div class="form-group">
-            <input type="password" class="form-control mb-2" id="confirmPassword" placeholder="Password Confirmation">
-          </div>
-        </div>
-        <div class="text-center">
-          <button type="submit" class="btn btn-primary btn-block btn-light myButton">Sign Up</button>
-        </div>
-      </form>
+      </div>
     </div>
     <script src="Router.js"></script>
   `;
@@ -39,34 +52,45 @@ const SignUpPage = () => {
     const login = loginInput.value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
-    console.log(login, password);
+
+    const form = document.getElementById('signup-form');
+
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      form.classList.add('was-validated');
+      return;
+    }
 
     // Vérifier si le login est un email valide
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(login)) {
       const errorMessage = document.createElement('div');
-      errorMessage.classList.add('alert', 'alert-danger');
+      errorMessage.classList.add('alert', 'alert-danger', 'mt-3');
       errorMessage.textContent = 'Le login doit être un email valide !';
-
-      const form = document.getElementById('signup-form');
-      form.insertAdjacentElement('afterend', errorMessage);
-
+      form.appendChild(errorMessage);
       return;
     }
 
     if (password !== confirmPassword) {
       const errorMessage = document.createElement('div');
-      errorMessage.classList.add('alert', 'alert-danger');
+      errorMessage.classList.add('alert', 'alert-danger', 'mt-3');
       errorMessage.textContent = 'La saisie du mot de passe et sa confirmation ne correspondent pas !';
-
-      const form = document.getElementById('signup-form');
-      form.insertAdjacentElement('afterend', errorMessage);
-
+      form.appendChild(errorMessage);
       return;
     }
 
     if (!login || !password) {
       console.error('Email and password are required!');
+      return;
+    }
+
+    // Vérifier les critères du mot de passe
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
+    if (!passwordRegex.test(password)) {
+      const errorMessage = document.createElement('div');
+      errorMessage.classList.add('alert', 'alert-danger', 'mt-3');
+      errorMessage.textContent = 'Votre mot de passe doit avoir au moins 8 caractères avec au minimum 1 majuscule et 1 chiffre.';
+      form.appendChild(errorMessage);
       return;
     }
 
@@ -79,8 +103,6 @@ const SignUpPage = () => {
         body: JSON.stringify({ login, password }),
       };
       const response = await fetch('/api/users/createUser', option);
-      console.log("vous êtes ici ...");
-      console.log(response);
 
       if (!response.ok) {
         throw new Error('Failed to create user');
