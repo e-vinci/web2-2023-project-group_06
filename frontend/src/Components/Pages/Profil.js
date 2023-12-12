7a2d2fd154a1d2043705ebfadbb9b74ff2e7fefb
Zhang Chuqi <zhangchuqi1220@hotmail.com>
2023-12-12 19:34:39 +0100

    profil user dynamic

--- frontend/src/Components/Pages/Profil.js
+++ frontend/src/Components/Pages/Profil.js
@@ -1,36 +1,37 @@
 async function ProfilUser() {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem('user'))[0];
   if (user) {
-    const main = document.querySelector('main');
+    console.log(user);
 
-    const response = await fetch('/api/listUsers');
-    if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
-    const data = await response.json();
+  console.log(user.profile_picture)
+
+    const main = document.querySelector('main');
 
     main.innerHTML = `
     <div class="container mt-5">
     <div class="card">
       <div class="row">
         <div class="col-md-4">
-          <img src="lien_vers_la_photo" class="img-fluid rounded-circle m-3" alt="Photo de profil">
+          <img src="${user.profile_picture}" class="img-fluid rounded-circle m-3" alt="Photo de profil" id="profilePicture">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">${data[1].name} ${data[1].surname}</h5>
+            <h5 class="card-title">${user.name} ${user.surname}</h5>
             <p>Description du profil :</p>
             <div class="card">
                 <div class="card-body">
-                    <p class="card-text"> ${data[1].profile_description} </p>
+                    <p class="card-text"> ${user.profile_description} </p>
                 </div>
             </div>
-            <p class="card-text"><small class="text-muted">Catégorie : ${data[1].category}</small></p>
-            <p class="card-text">Score :<span class="badge badge-success">${data[1].quizz_score}</span></p>
+            <p class="card-text"><small class="text-muted">Catégorie : ${user.category}</small></p>
+            <p class="card-text">Score :<span class="badge badge-success">${user.quizz_score}</span></p>
           </div>
         </div>
       </div>
     </div>
   </div>
     `;
+
   } else {
       const main = document.querySelector('main');
       main.innerHTML = `
